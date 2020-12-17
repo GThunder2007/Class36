da97ffd4a8afee71878e79588f5095d35f561445
GThunder2007 <71527769+GThunder2007@users.noreply.github.com>
2020-12-17 10:51:47 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,22 @@
+var gameState = 0;
+var playerCount = 0;
+var form,game,player;
+
+
+var database;
+
+
+
+function setup(){
+    createCanvas(500,500);
+    database = firebase.database()
+   game = new Game()
+   game.getState();
+   game.start();
+}
+
+function draw(){
+    background("white");
+    
+    drawSprites();
+}
